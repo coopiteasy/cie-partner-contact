65cd967c2d863dfebec168061367c1e8abc9852c
Simon Hick <sim@coopiteasy.be>
2025-09-09 16:42:03 +0200

    [MIG] partner_personal_information_access: Migration to 16.0

--- partner_personal_information_access/__manifest__.py
+++ partner_personal_information_access/__manifest__.py
@@ -4,13 +4,11 @@
     "name": "Personal Information Access",
     "summary": "This module restricts access to personal "
     "fields to users of group 'Access to Private Addresses'.",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/coopiteasy/cie-partner-contact",
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
     "depends": [
         "base",
         "mail",
